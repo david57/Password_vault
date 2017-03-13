038c2dd8765f7f00b556f6743ef0c8f71c502462
TTDanteTT <daviddb@sfr.fr>
2017-03-13 18:07:10 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from database.test_database import *
+from rules_gen.test_rules_gen import *
+from random_gen.test_random_gen import *
+from cor_generator.test_cor_generator import *
+from graphical.test_graphical import *
+
+#test de la database
+#test_database()
+
+#test rules_gen
+#test_rule_gen()
+
+#test random generation
+#test_random_gen()
+
+#test cor_generation
+#Test_cor_generator()
+
+#test graphical_interface
+test_graphical()
